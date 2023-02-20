8fcef06ae9891df9dd85adc1127e78eed777384d
agreedrn <rishinachnani1@gmail.com>
2023-02-19 20:21:23 -0500

    optimizations made

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-"# pdf-parser" 
+This is a PDF Parser that can extract data from Wealthsimple PDFs, to get stock buying data from a user, and possibly compare it to current statistics using APIs (in the future).
--- main.py
+++ main.py
@@ -1,48 +1,43 @@
 from PyPDF2 import PdfReader
-import yfinance as yf #new, used to grab yahoo finanace api data
 import subprocess
 import os
 
-def extract_share_pdf_values():
-    for (root,dirs,files) in os.walk('/'):
-        if "$Recycle.Bin" in root: #do not want to search the recycle bin
-            continue
-        if "wealthsimple.pdf" in files:
-            pdf_path = root+"/wealthsimple.pdf"
-            break
-    else:
-        raise Exception("Pdf File Not Found. Make sure it is on your system and named 'wealthsimple.pdf'.")
-
-    print(f"Found PDF: {pdf_path}")
+def extract_share_pdf_values(pdf_path):
 
     reader = PdfReader(pdf_path)
     page = reader.pages[0] #page index, 0 = page 1
-    split_pdf_text = page.extract_text().split("\n")
+    split_pdf_text = page.extract_text().split("\n") #split the text into a list, by line
 
-    detail_value = lambda detail: [split_pdf_text[index].split(detail) for index, line in enumerate(split_pdf_text) if detail in line][0][1]
-    qps_data = [split_pdf_text[index+1].split(" ") for index, line in enumerate(split_pdf_text) if line == "Quantity Price per Share Security"][0] #qps_data = Quantity, Price & Share data
+    # Get the value of a detail (e.g. Gross, Commission, etc.)
+    def detail_value(detail):
+        for index, line in enumerate(split_pdf_text): #iterate through the list, while grabbing the index
+            if detail in line:
+                return split_pdf_text[index].split(detail)[1] #peform some string magic to get the value
 
-    quantity = float(qps_data[0])
-    buy_price = float(qps_data[1].replace('$', ''))
+    # Get the quantity, buy price, currency flag, and ticker
+    for index, line in enumerate(split_pdf_text):
+        if line == "Quantity Price per Share Security":
+            qps_data = split_pdf_text[index+1].split(" ") #peform some string magic to get a list of the quantity, buy price, currency flag, and ticker
+            break
+
+    quantity = qps_data[0]
+    buy_price = qps_data[1]
     currency_flag = qps_data[2]
     ticker = qps_data[3]
-    stock_info = yf.Ticker(ticker).info
-    stock_name = stock_info["longName"]
     gross = detail_value("Gross: ")
     commission = detail_value("Commission: ")
     net_amount = detail_value("Net Amount: ")
     for_settlement_on = detail_value("For Settlement On: ")
 
-    return quantity, buy_price, currency_flag, ticker, stock_info, stock_name, gross, commission, net_amount, for_settlement_on
+    return quantity, buy_price, currency_flag, ticker, gross, commission, net_amount, for_settlement_on
 
 if __name__ == "__main__":
-    quantity, buy_price, currency_flag, ticker, stock_info, stock_name, gross, commission, net_amount, for_settlement_on = extract_share_pdf_values()
+    quantity, buy_price, currency_flag, ticker, gross, commission, net_amount, for_settlement_on = extract_share_pdf_values('wealthsimple.pdf')
     
     print(f"Quantity: {quantity}")
     print(f"Buy Price: {buy_price}")
     print(f"Currency Flag: {currency_flag}")
     print(f"Ticker: {ticker}")
-    print(f"Stock Name: {stock_name}")
     print(f"Gross: {gross}")
     print(f"Commission: {commission}")
     print(f"Net Amount: {net_amount}")
